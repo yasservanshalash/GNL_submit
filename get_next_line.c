e06471229fe0392488964397856566dcea20ed5e
Ubuntu <ubuntu@ip-172-31-45-112.us-east-2.compute.internal>
2023-12-06 09:52:45 +0000

    done

--- get_next_line.c
+++ get_next_line.c
@@ -11,98 +11,115 @@
 # define BUFFER_SIZE 1000000
 #endif
 
-void leftover_logic(char **line, char **leftover, int *i, int *j) {
-    if (*leftover != NULL) {
-        while ((*leftover)[*j] != '\n' && (*leftover)[*j] != '\0') {
-            (*line)[(*i)++] = (*leftover)[(*j)++];
-        }
-        if ((*leftover)[*j] == '\n') {
-            *leftover = strdup(*leftover + *j + 1);
-            (*line)[(*i)++] = '\n';
-            (*line)[*i] = '\0';
-        }
-        // Handle case where leftover doesn't contain a newline
-        if ((*leftover)[*j] == '\0') {
-            free(*leftover);
-            *leftover = NULL;
-        }
-    }
+void	leftover_logic(char **line, char **leftover, int *i, int *j)
+{
+	char	*temp;
+
+	if (*leftover != NULL)
+	{
+		while ((*leftover)[*j] != '\n' && (*leftover)[*j] != '\0')
+		{
+			(*line)[(*i)++] = (*leftover)[(*j)++];
+		}
+		if ((*leftover)[*j] == '\n')
+		{
+			temp = strdup(*leftover + *j + 1);
+			free(*leftover);
+			*leftover = temp;
+			(*line)[(*i)++] = '\n';
+			(*line)[*i] = '\0';
+		}
+		else
+		{
+			free(*leftover);
+			*leftover = NULL;
+		}
+	}
 }
 
-char *line_logic(int fd, char **line, int *i, int *j) {
-    int read_chars;
-    char *leftover;
-    char *new_line;
-
-    read_chars = read(fd, *line + *i, BUFFER_SIZE - *i);
-    while (read_chars > 0) {
-        *i += read_chars;
-        if (strchr(*line, '\n')) {
-            *j = *i - read_chars;
-            while (*j < *i && (*line)[*j] != '\n') {
-                (*j)++;
-            }
-            if (*j < *i) {
-                leftover = strdup(*line + *j + 1);
-                (*line)[(*j)++] = '\n';
-                (*line)[*j] = '\0';
-                return leftover;
-            }
-        }
-        // Replace realloc with free and malloc
-        new_line = malloc((*i + BUFFER_SIZE + 1) * sizeof(char));
-        if (new_line == NULL) {
-            free(*line);  // Free the original line buffer
-            return NULL;
-        }
-        memcpy(new_line, *line, *i);
-        free(*line);
-        *line = new_line;
-        memset(*line + *i, 0, BUFFER_SIZE + 1);
-        read_chars = read(fd, *line + *i, BUFFER_SIZE);
-    }
-    return NULL;
+char	*leftover_in_line_logic(char **leftover, char **line, int *j)
+{
+	*leftover = strdup(*line + *j + 1);
+	if (*leftover == NULL)
+	{
+		free(*line);
+		return (NULL);
+	}
+	(*line)[(*j)++] = '\n';
+	(*line)[*j] = '\0';
+	return (*leftover);
 }
 
+char	*line_logic(int fd, char **line, int *i, int *j)
+{
+	int		read_chars;
+	char	*leftover;
 
-char *get_next_line(int fd) {
-    char *line;
-    static char *leftover = NULL;
-    int i = 0;
-    int j = 0;
-
-    if (fd < 0)
-        return NULL;
-    line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-    bzero(line, BUFFER_SIZE + 1);
-    if (line == NULL)
-        return NULL;
-    leftover_logic(&line, &leftover, &i, &j);
-    if (strchr(line, '\n'))
-        return line;
-    leftover = line_logic(fd, &line, &i, &j);
-    if (i > 0) {
-        line[i] = '\0';
-        return line;
-    } else {
-        free(line);
-        return NULL;
-    }
+	read_chars = read(fd, *line + *i, BUFFER_SIZE - *i);
+	while (read_chars > 0)
+	{
+		*i += read_chars;
+		if (strchr(*line, '\n'))
+		{
+			*j = *i - read_chars;
+			while (*j < *i && (*line)[*j] != '\n')
+				(*j)++;
+			if (*j < *i)
+			{
+				return (leftover_in_line_logic(&leftover, line, j));
+			}
+		}
+		*line = realloc(*line, (*i + BUFFER_SIZE + 1) * sizeof(char));
+		if (*line == NULL)
+			return (NULL);
+		bzero(*line + *i, BUFFER_SIZE + 1);
+		read_chars = read(fd, *line + *i, BUFFER_SIZE);
+	}
+	return (NULL);
 }
 
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*leftover = NULL;
+	int			i;
+	int			j;
 
-// int main(void)
-// {
+	i = 0;
+	j = 0;
+	if (fd < 0)
+		return (NULL);
+	line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	bzero(line, BUFFER_SIZE + 1);
+	leftover_logic(&line, &leftover, &i, &j);
+	if (strchr(line, '\n'))
+	{
+		return (line);
+	}
+	leftover = line_logic(fd, &line, &i, &j);
+	if (i > 0)
+		return (line);
+	else
+	{
+		free(line);
+		return (NULL);
+	}
+}
+
+// int main(void) {
 //     int fd;
-
 //     fd = open("text.txt", O_RDONLY);
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
+//     if (fd < 0) {
+//         perror("Error opening file");
+//         return (1);
+//     }
+
+//     char *result;
+//     while ((result = get_next_line(fd)) != NULL) {
+//         printf("%s", result);
+//         free(result);  // Free each line obtained from get_next_line
+//     }
+
 //     close(fd);
 //     return (0);
 // }
